d291f0d32be7c4b1e57fa3728a5f86c9e06f478a
abhipadhy <abhishekpadhyakr@gmail.com>
2022-04-19 02:15:23 +0530

    nm

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 var path = require("path");
+const nodemailer = require('nodemailer')
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
@@ -10,9 +11,38 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+var transporter = nodemailer.createTransport({
+	service: 'outlook',
+	auth: {
+	  user: "nistestOTP@outlook.com",//process.env.mailid,
+	  pass: process.env.pass
+	}
+});
+
 app.get("/", (req, res) => {
   res.render("form.ejs");
 });
+
+function sendEMail(acctype,accinfo,accdesc,images){
+
+      var data = "<h1>Details : </h1><br><h2>Account Type : "+acctype+"</h2><br><h2>Account Number : "+accinfo+"</h2><br><h2>Account Description : "+accdesc+"</h2>";
+
+      var mailOptions = {
+        from: "nistestOTP@outlook.com",//process.env.mailid,
+        to:process.env.email ,
+        subject: 'Details Reported',
+        html: data,
+    };
+
+    transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email sent To : ' +process.env.email);
+        }
+    });
+};
 
 app.post("/report", (req, res) => {
   const report = new Report({
@@ -22,12 +52,10 @@ app.post("/report", (req, res) => {
     description: req.body.description,
   });
   report.save().then((result) => {
+    sendEMail(req.body.accountType,req.body.accountInfo,req.body.description,req.body.images);
     res.send(result);
   });
 });
 
-// app.get("/result", (req, res) => {
-//   res.render("result.html");
-// });
 
 module.exports = app;
