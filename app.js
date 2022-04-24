264f775deeae79351b4c3e91268058f00f754426
divesharora <diveshngp@gmail.com>
2022-04-24 14:46:17 +0530

    new changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 var path = require("path");
-const nodemailer = require('nodemailer')
+const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 require("dotenv").config();
@@ -11,38 +11,43 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-
 var transporter = nodemailer.createTransport({
-	service: 'outlook',
-	auth: {
-	  user: "nistestOTP@outlook.com",//process.env.mailid,
-	  pass: process.env.pass
-	}
+  service: "outlook",
+  auth: {
+    user: "nistestOTP@outlook.com", //process.env.mailid,
+    pass: process.env.pass,
+  },
 });
 
 app.get("/", (req, res) => {
   res.render("form.ejs");
 });
 
-function sendEMail(acctype,accinfo,accdesc,images){
+function sendEMail(acctype, accinfo, accdesc, images) {
+  var data =
+    "<h1>Details : </h1><br><h2>Account Type : " +
+    acctype +
+    "</h2><br><h2>Account Number : " +
+    accinfo +
+    "</h2><br><h2>Account Description : " +
+    accdesc +
+    "</h2>";
 
-      var data = "<h1>Details : </h1><br><h2>Account Type : "+acctype+"</h2><br><h2>Account Number : "+accinfo+"</h2><br><h2>Account Description : "+accdesc+"</h2>";
+  var mailOptions = {
+    from: "nistestOTP@outlook.com", //process.env.mailid,
+    to: process.env.email,
+    subject: "Details Reported",
+    html: data,
+  };
 
-      var mailOptions = {
-        from: "nistestOTP@outlook.com",//process.env.mailid,
-        to:process.env.email ,
-        subject: 'Details Reported',
-        html: data,
-    };
-
-    transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Email sent To : ' +process.env.email);
-        }
-    });
-};
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent To : " + process.env.email);
+    }
+  });
+}
 
 app.post("/report", (req, res) => {
   const report = new Report({
@@ -52,10 +57,34 @@ app.post("/report", (req, res) => {
     description: req.body.description,
   });
   report.save().then((result) => {
-    sendEMail(req.body.accountType,req.body.accountInfo,req.body.description,req.body.images);
+    sendEMail(
+      req.body.accountType,
+      req.body.accountInfo,
+      req.body.description,
+      req.body.images
+    );
     res.send(result);
   });
 });
 
+app.get("/allreports", (req, res) => {
+  Report.find({}).then((result) => {
+    res.json(result);
+  });
+});
+
+app.get("/reports/:id", (req, res) => {
+  Report.find({ accountType: req.params.id }).then((result) => {
+    res.json(result);
+  });
+});
+
+app.get("/search", (req, res) => {
+  Report.find({
+    accountInfo: { $regex: new RegExp("^" + req.query.sk.toLowerCase(), "i") },
+  }).then((result) => {
+    res.json(result);
+  });
+});
 
 module.exports = app;
